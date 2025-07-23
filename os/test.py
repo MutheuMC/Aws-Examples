2d6364b49120348e34ca42513125bc538f6daba5
DekutSiemens <smscp.kenya@dkut.ac.ke>
2025-07-23 10:59:54 +0300

    new

---
+++ os/test.py
@@ -0,0 +1,14 @@
+import os
+
+os.getcwd
+
+print("These are your envirnmental variables \n", os.environ)
+
+# for dirpath, dirnames, filenames in os.walk('c:/Users/admin/Documents/Projects/AWS-EXAMPLES'):
+#     print("Current path", dirpath)
+#     print("Directories", dirnames)
+#     print('Files', filenames)
+#     print()
+
+
+
